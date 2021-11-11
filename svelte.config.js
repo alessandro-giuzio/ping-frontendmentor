bde4967f9d013807741692b815126c2fb3854d76
Alessandro Giuzio <giuzio@icloud.com>
2021-11-11 16:42:40 +0100

    add adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
-import preprocess from "svelte-preprocess";
-
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 const config = {
+  kit: {
+    adapter: adapter(),
+    target: '#svelte',
+  },
   preprocess: [
     preprocess({
       postcss: true,
